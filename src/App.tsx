d97de82c32bec4c4037e10a9a9c3335edce36394
ThierryWelling <thierry.designer.oficial@gmail.com>
2025-01-17 20:40:35 -0300

    fix: ajustando configurações para deploy no Vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { CssBaseline } from '@mui/material';
 import { store } from './store';
@@ -12,61 +12,43 @@ import UserManagement from './components/Users/UserManagement';
 import CollaboratorDetails from './components/Dashboard/CollaboratorDetails';
 import PrivateRoute from './components/PrivateRoute';
 
-const router = createBrowserRouter([
-  {
-    path: "/login",
-    element: <Login />
-  },
-  {
-    path: "/tasks",
-    element: (
-      <PrivateRoute>
-        <MainLayout>
-          <TaskList />
-        </MainLayout>
-      </PrivateRoute>
-    )
-  },
-  {
-    path: "/dashboard",
-    element: (
-      <PrivateRoute>
-        <MainLayout>
-          <Dashboard />
-        </MainLayout>
-      </PrivateRoute>
-    )
-  },
-  {
-    path: "/users",
-    element: (
-      <PrivateRoute>
-        <MainLayout>
-          <UserManagement />
-        </MainLayout>
-      </PrivateRoute>
-    )
-  },
-  {
-    path: "/collaborator/:userId",
-    element: (
-      <PrivateRoute>
-        <CollaboratorDetails />
-      </PrivateRoute>
-    )
-  },
-  {
-    path: "/",
-    element: <Navigate to="/login" replace />
-  }
-]);
-
 const App: React.FC = () => {
   return (
     <Provider store={store}>
       <SettingsProvider>
         <CssBaseline />
-        <RouterProvider router={router} />
+        <Router>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/tasks" element={
+              <PrivateRoute>
+                <MainLayout>
+                  <TaskList />
+                </MainLayout>
+              </PrivateRoute>
+            } />
+            <Route path="/dashboard" element={
+              <PrivateRoute>
+                <MainLayout>
+                  <Dashboard />
+                </MainLayout>
+              </PrivateRoute>
+            } />
+            <Route path="/users" element={
+              <PrivateRoute>
+                <MainLayout>
+                  <UserManagement />
+                </MainLayout>
+              </PrivateRoute>
+            } />
+            <Route path="/collaborator/:userId" element={
+              <PrivateRoute>
+                <CollaboratorDetails />
+              </PrivateRoute>
+            } />
+            <Route path="/" element={<Navigate to="/login" replace />} />
+          </Routes>
+        </Router>
       </SettingsProvider>
     </Provider>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -12,29 +12,17 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true,
-    chunkSizeWarningLimit: 800,
+    sourcemap: false,
     rollupOptions: {
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom', 'react-router-dom'],
           mui: ['@mui/material', '@mui/icons-material'],
-          redux: ['@reduxjs/toolkit', 'react-redux'],
-          charts: ['recharts'],
-          utils: ['axios', 'notistack', 'uuid'],
         },
       },
     },
   },
   server: {
     port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
   },
 }) 
